9e8f4463ab436acfdc6143da4f122b54c6586182
fer2ap <pinto.faa@gmail.com>
2023-10-04 20:25:36 -0300

    Creating actions

--- main.go
+++ main.go
@@ -11,6 +11,39 @@ func main() {
 	// task1 := Task{"Test operation 1", false}
 	// task2 := Task{"Save file 2", false}
 	// todolist := []Task{task1, task2}
+
+	args := os.Args
+	fmt.Println(args)
+	if len(args) == 1 || args[1] == "list" {
+		listTasks()
+	}
+	if args[1] == "create" {
+		createTask()
+	}
+	if args[1] == "deleteAll" {
+		deleteAll()
+	}
+}
+
+func deleteAll() {
+	filePath, err := util.GetFilePath("tasks")
+	if err != nil {
+		panic(err)
+	}
+	exists, err := util.FileExists(filePath)
+	if err != nil {
+		panic(err)
+	}
+	if exists {
+		err = saveTasks([]Task{})
+		if err != nil {
+			panic(err)
+		}
+	}
+	fmt.Println("Tasks deleted")
+}
+
+func listTasks() {
 	filePath, err := util.GetFilePath("tasks")
 	if err != nil {
 		panic(err)
@@ -25,13 +58,35 @@ func main() {
 			panic(err)
 		}
 		printMenu(tasks)
+	} else {
+		fmt.Println("There are no tasks here. Use the create method to create your first one, i.e. gdi create")
+	}
+}
+
+func createTask() {
+	filePath, err := util.GetFilePath("tasks")
+	if err != nil {
+		panic(err)
+	}
+	exists, err := util.FileExists(filePath)
+	if err != nil {
+		panic(err)
+	}
+	if exists {
+		fmt.Println("File recovered")
+		tasks, err := recoverTasks()
+		if err != nil {
+			panic(err)
+		}
 		task := createNewTask()
+		fmt.Println("Creating task: ", task)
 		err = saveTasks(append(tasks, task))
 		if err != nil {
 			panic(err)
 		}
 		printMenu(append(tasks, task))
 	} else {
+		fmt.Println("File created")
 		task := createNewTask()
 		tasks := []Task{task}
 		err = saveTasks(tasks)
@@ -65,16 +120,23 @@ func printMenu(tasks []Task) {
 func recoverTasks() ([]Task, error) {
 	filePath, err := util.GetFilePath("tasks")
 	if err != nil {
+		fmt.Println("Error getting file path")
 		return nil, err
 	}
 	content, err := os.ReadFile(filePath)
 	if err != nil {
+		fmt.Println("Error reading file")
 		return nil, err
 	}
 	var tasks []Task
-	err = json.Unmarshal(content, &tasks)
-	if err != nil {
-		return nil, err
+	if len(content) == 0 {
+		tasks = []Task{}
+	} else {
+		err = json.Unmarshal(content, &tasks)
+		if err != nil {
+			fmt.Println("Error unmarshalling file")
+			return nil, err
+		}
 	}
 	return tasks, nil
 }
@@ -97,7 +159,6 @@ func saveTasks(tasks []Task) error {
 	if err != nil {
 		return err
 	}
-	fmt.Println("Task saved")
 	// fmt.Println(string(j))
 	return nil
 }
